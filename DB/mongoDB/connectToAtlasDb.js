5058800acaf3433f202e0f7cbedb130904613025
Derpy666 <noamgl2002@gmail.com>
2025-11-24 11:41:55 +0200

    .

--- DB/mongoDB/connectToAtlasDb.js
+++ DB/mongoDB/connectToAtlasDb.js
@@ -3,11 +3,11 @@ const chalk = require("chalk");
 const config = require("config");
 
 const userName = config.get("DB_NAME");
-const password = config.get("DB_PASSWORD");
+const password = process.env.DB_PASSWORD
 
 mongoose
   .connect(
-    `mongodb+srv://${userName}:${password}@hackeru-cluster.y5spzbw.mongodb.net/`
+    `mongodb+srv://${userName}:${password}@cluster0.ke8szkn.mongodb.net/noam`
   )
   .then(() => console.log(chalk.magentaBright("Connect To Atlas MongoDB!")))
   .catch((error) => {
